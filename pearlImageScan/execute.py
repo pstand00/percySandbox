5c84ec60c563302a31355a699ecc17d10174a20b
pstand00 <peterstandbridge@yahoo.com>
2022-05-15 20:55:06 -0400

    finsihed the email part of the script

--- pearlImageScan/execute.py
+++ pearlImageScan/execute.py
@@ -44,71 +44,82 @@ emailUser = pwdDf[['personalGmail']].filter(like = 'user', axis = 0).to_string(i
 emailPw = pwdDf[['personalGmail']].filter(like = 'password', axis = 0).to_string(index=False, header= False).strip()
 
 
-# scan file names and select one at random 
-# ========================================================
-imageDir = wFm + '\\Documents\\images\\pearlPics'
-imageList = os.listdir(imageDir)
-imageDf = pd.DataFrame({'fileName':[], 'randoNum':[]})
-for i in imageList:
-    print(i)
-    rando = random.random()
-    imageDf = imageDf.append(pd.DataFrame({'fileName':[i], 'randoNum':[rando]}))
+try:
+    # scan file names and select one at random 
+    # ========================================================
+    imageDir = wFm + '\\Documents\\images\\pearlPics'
+    imageList = os.listdir(imageDir)
+    imageDf = pd.DataFrame({'fileName':[], 'randoNum':[]})
+    for i in imageList:
+        print(i)
+        rando = random.random()
+        imageDf = imageDf.append(pd.DataFrame({'fileName':[i], 'randoNum':[rando]}))
 
-# pick the max random number 
-imageDfOrder = imageDf.sort_values(by = 'randoNum').head(1)
-fileName = imageDfOrder['fileName'].to_string(index=False, header= False).strip()
-filePath = imageDir +'\\' + fileName
-os.chdir(imageDir)
+    # pick the max random number 
+    imageDfOrder = imageDf.sort_values(by = 'randoNum').head(1)
+    fileName = imageDfOrder['fileName'].to_string(index=False, header= False).strip()
+    filePath = imageDir +'\\' + fileName
+    os.chdir(imageDir)
 
+    # resize the image so that it can be sent as an email 
+    # ========================================================
+    from PIL import Image # pip install Pillow
+    # My image is a 200x374 jpeg that is 102kb large
+    foo = Image.open(filePath)
+    downsize1 = foo.size[0] * 0.2
+    downsize2 = foo.size[1] * 0.2
+    foo2 = foo.resize((int(downsize1),int(downsize2)))
+    # I downsize the image with an ANTIALIAS filter (gives the highest quality)
+    # The saved downsized image size is 24.8kb
+    foo2.save(imageDir +'\\temp.png',optimize=True,quality=95)
+    # The saved downsized image size is 22.9kb
+    newFile = 'temp.png'
 
-# resize the image so that it can be sent as an email 
-# ========================================================
-from PIL import Image # pip install Pillow
-# My image is a 200x374 jpeg that is 102kb large
-foo = Image.open(filePath)
-downsize1 = foo.size[0] * 0.25
-downsize2 = foo.size[1] * 0.25
-foo2 = foo.resize((int(downsize1),int(downsize2)))
- # I downsize the image with an ANTIALIAS filter (gives the highest quality)
- # The saved downsized image size is 24.8kb
-foo2.save(imageDir +'\\temp.png',optimize=True,quality=95)
-# The saved downsized image size is 22.9kb
-newFile = 'temp.png'
+    # send message with attachment 
+    # ========================================================
+    # emailTxt = 'This is a test message 2 '
+    emailTxt = 'Happy ' + currentDayOfWeek + ' from Ms. Pearl'
+    msg = MIMEMultipart("alternative")
+    # msg["Subject"] = "SubjectLine"
+    msg["From"] = fromEmail
+    msg["To"] = toEmail
+    bodyMime = MIMEText(emailTxt)
+    msg.attach(bodyMime)
+    # Open PDF file in binary mode
+    with open(newFile, "rb") as attachment:
+        # Add file as application/octet-stream
+        # Email client can usually download this automatically as attachment
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
 
+    # Encode file in ASCII characters to send by email    
+    encoders.encode_base64(part)
 
-# send message with attachment 
-# ========================================================
-# emailTxt = 'This is a test message 2 '
-emailTxt = 'Happy ' + currentDayOfWeek + ' from Ms. Pearl'
-msg = MIMEMultipart("alternative")
-# msg["Subject"] = "SubjectLine"
-msg["From"] = fromEmail
-msg["To"] = toEmail
-bodyMime = MIMEText(emailTxt)
-msg.attach(bodyMime)
-# Open PDF file in binary mode
-with open(newFile, "rb") as attachment:
-    # Add file as application/octet-stream
-    # Email client can usually download this automatically as attachment
-    part = MIMEBase("application", "octet-stream")
-    part.set_payload(attachment.read())
+    # Add header as key/value pair to attachment part
+    part.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {newFile}",
+    )
 
-# Encode file in ASCII characters to send by email    
-encoders.encode_base64(part)
+    # Add attachment to message and convert message to string
+    msg.attach(part)
 
-# Add header as key/value pair to attachment part
-part.add_header(
-    "Content-Disposition",
-    f"attachment; filename= {newFile}",
-)
+    # send the actual note
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.login(emailUser, emailPw)
+    server.sendmail(fromEmail, toEmail, msg.as_string())
 
-# Add attachment to message and convert message to string
-msg.attach(part)
-
-# send the actual note
-server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-server.login(emailUser, emailPw)
-server.sendmail(fromEmail, toEmail, msg.as_string())
-
-# delete the temp pic 
-os.remove(imageDir +'\\temp.png')
+    # delete the temp pic 
+    os.remove(imageDir +'\\temp.png')
+except:
+    emailTxt = 'For some reason, Ms. Pearl does not say hello today'
+    msg = MIMEMultipart("alternative")
+    # msg["Subject"] = "SubjectLine"
+    msg["From"] = fromEmail
+    msg["To"] = toEmail
+    bodyMime = MIMEText(emailTxt)
+    msg.attach(bodyMime)
+    # send the actual note
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.login(emailUser, emailPw)
+    server.sendmail(fromEmail, toEmail, msg.as_string())
