78c0e79e3789d00c47e1b269acd91b0ed54d414d
pstand00 <peterstandbridge@yahoo.com>
2022-06-19 13:06:54 -0400

    tested out pyppeteer

--- devProjects/pythonWebBrowsing/webBrowserWs.py
+++ devProjects/pythonWebBrowsing/webBrowserWs.py
@@ -29,6 +29,59 @@ system(chromePath +  " -incognito -new-window "  + urlText)
 # > this is an example of one that would be a pro
 
 
-# google chrome api 
+
+
+# a package called "pupateer" [called pypateer for python]
+# this one works pretty well! gonna use it for now
+# ============================================================
+
+import pyppeteer # pip install pyppeteer
+
+
+browser = await launch()
+page = await browser.newPage()
+await page.goto('https://espn.com')
+
+content = await page.evaluate('document.body.textContent', force_expr=True)
+
+screenshotPath = "C:\\Users\\Pete\\Downloads\\"
+
+# update the working directory to downloads 
+import os 
+os.chdir(screenshotPath)
+os.getcwd()
+
+# the below works!! but does not actually open the browser [which is prob what we want ]
+browser = await launch()
+page = await browser.newPage()
+await page.goto('http://espn.com')
+await page.screenshot({'path': 'example.png'})
+await browser.close()
+
+# try making the window bigger for the screenshot 
+# this one works too! 
+browser = await launch()
+page = await browser.newPage()
+await page.setViewport({"width": 1600, "height": 900})
+await page.goto('http://espn.com')
+await page.screenshot({'path': 'example2.png'})
+await browser.close()
+
+# try one more time with a zoom out 
+# couldn't figure out a zoom out, but have the ability to screenshot the entire "scrollable page" 
+browser = await launch()
+page = await browser.newPage()
+await page.setViewport({"width": 1600, "height": 900})
+await page.goto('http://espn.com')
+await page.screenshot({'path': 'example3.png', 'fullPage':True})
+await browser.close()
+
+# if the zoom out works, then test it's runability via task scheduler 
+
+
+
+
+
+# google chrome api [still need to research this one]
 # ============================================================
 # using the peter.standbridge@gmail.com account
