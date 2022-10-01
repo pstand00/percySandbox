1b6254f5ab0bf0939d74d24817863f3eb11a2d44
pstand00 <peterstandbridge@yahoo.com>
2022-10-01 14:02:38 -0400

    creating file to test email functionality on mac

---
+++ appIconRefreh/testEmail.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  1 13:21:32 2022
+
+@author: pete
+"""
+
+# test file to make sure i can run stuff from the command prompt 
+
+# refer to pw file 
+# send email message 
+# 
